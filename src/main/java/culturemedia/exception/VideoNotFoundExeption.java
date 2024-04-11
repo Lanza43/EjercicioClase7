b03ac36f170959c8f83073c893920829ebe21900
Lanza43 <mateo.vasquezg@udea.edu.co>
2024-04-10 20:00:13 -0500

    V 1.3

---
+++ src/main/java/culturemedia/exception/VideoNotFoundExeption.java
@@ -0,0 +1,13 @@
+package culturemedia.exception;
+
+import java.text.MessageFormat;
+
+
+public class VideoNotFoundExeption extends CultureMediaException{
+    public VideoNotFoundExeption() {
+        super("Video not found");
+    }
+    public VideoNotFoundExeption(String title) {
+        super(MessageFormat.format("Video not found with title: {0}", title));
+    }
+}
