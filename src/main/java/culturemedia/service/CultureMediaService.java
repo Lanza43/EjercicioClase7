698429e72b8aa19878bb4a58fc8f053959efa101
Lanza43 <mateo.vasquezg@udea.edu.co>
2024-04-23 11:08:01 -0500

    V 1.8

--- src/main/java/culturemedia/service/CultureMediaService.java
+++ src/main/java/culturemedia/service/CultureMediaService.java
@@ -5,6 +5,8 @@ import culturemedia.exception.VideoNotFoundExeption;
 
 public interface CultureMediaService {
     List<Video> findAllVideos() throws VideoNotFoundExeption;
+    List<Video> find(String title) throws VideoNotFoundExeption;
+    List<Video> find(double fromDuration, double toDuration) throws VideoNotFoundExeption;
     Video add(Video video);
     View add(View view);
 }
