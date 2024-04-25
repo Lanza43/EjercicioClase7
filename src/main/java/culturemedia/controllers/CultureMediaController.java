80ba5d4a0b4061dcdd21411a3657a031002aca46
Lanza43 <mateo.vasquezg@udea.edu.co>
2024-04-24 19:33:35 -0500

    V 1.9.2

--- src/main/java/culturemedia/controllers/CultureMediaController.java
+++ src/main/java/culturemedia/controllers/CultureMediaController.java
@@ -1,24 +1,23 @@
 package culturemedia.controllers;
 
-import java.util.*;
+import java.util.List;
 
 import culturemedia.exception.VideoNotFoundException;
 import culturemedia.model.Video;
-import culturemedia.service.impl.CultureMediaServiceImpl;
+import culturemedia.service.CultureMediaService;
 
 public class CultureMediaController {
 
-	private final CultureMediaServiceImpl cultureMediaService;
+	private final CultureMediaService cultureMediaService;
 
 
-	public CultureMediaController(CultureMediaServiceImpl cultureMediaService) {
+	public CultureMediaController(CultureMediaService cultureMediaService) {
 		this.cultureMediaService = cultureMediaService;
 	}
 
 
-	public List<Video> find_allVideos() throws VideoNotFoundException {
-		List<Video> videos = null;
-		videos = cultureMediaService.findAllVideos();
+	public List<Video> findAllVideos() throws VideoNotFoundException {
+		List<Video> videos = cultureMediaService.findAllVideos();
 		return videos;
 	}
 }
